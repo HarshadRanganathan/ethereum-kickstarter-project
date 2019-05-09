68d98759949e25685b3a1d750dd4fe814e39db3f
RanganathanH <ranganathanh@dnb.com>
2019-05-09 15:58:02 +0100

    Routing handler

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Card, Button } from 'semantic-ui-react';
 import factory from '../ethereum/factory';
 import Layout from '../components/Layout';
+import { Link } from '../routes';
 
 class CampaignIndex extends Component {
 
@@ -14,7 +15,11 @@ class CampaignIndex extends Component {
         const items = this.props.campaigns.map(address => {
             return {
                 header: address,
-                description: <a>View Campaign</a>,
+                description: (
+                    <Link route={`/campaigns/${address}`}>
+                        <a>View Campaign</a>
+                    </Link>
+                ),
                 fluid: true
             }
         });
@@ -26,12 +31,16 @@ class CampaignIndex extends Component {
             <Layout>
                 <div>
                     <h3>Open Campaigns</h3>
-                    <Button 
-                        floated="right"
-                        content="Create Campaign"
-                        icon="add circle"
-                        primary
-                    />
+                    <Link route="/campaigns/new">
+                        <a>
+                            <Button 
+                                floated="right"
+                                content="Create Campaign"
+                                icon="add circle"
+                                primary
+                            />
+                        </a>
+                    </Link>
                     {this.renderCampaigns()}
                 </div>
             </Layout>
